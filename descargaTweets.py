da19356a567d316aef34953b64547e1f0396dcb0
Daniel Zermeño <daniel.zermeno@softwaria.mx>
2020-09-10 13:03:44 -0500

    Se agrega dinamismo en el programa ahora se pueden buscar las palabras que se deseen

--- descargaTweets.py
+++ descargaTweets.py
@@ -1,7 +1,6 @@
 #Libraries
 import os
 import tweepy as tw
-import pandas as pd
 
 #API KEYS (Don't Share with anyone)
 consumer_key= 'f6u2qHFTUmJdP6R8p7cpdC74z'
@@ -9,9 +8,6 @@ consumer_secret= 'UTW82wztu0NOwg3wl6wTA04CR3V4DseNPSBWjb6OsJRFfGxkb6'
 access_token= '574202891-dAiMTfGqTlNCiKHNTPbDJqQ7gwBbQfc0tEUBz7VW'
 access_token_secret= 'bKhB1HVNMO7pJ3X4CdBML9Xbz1nU2lwH04nGbf8Fy8KhM'
 
-#Terms and date that will work as a Filter
-search_words = ['#UAA']
-date_since = '2018-11-16'
 
 #Function that made the connection with Twitter API
 def connection():
@@ -20,24 +16,48 @@ def connection():
   return tw.API(auth, wait_on_rate_limit=True)
 
 def main():
+  #Terms and date that will work as a Filter
+  search_words = ['#UAA']
+  date_since = '2018-11-16'
+  amount_of_results = 5
+
+  option = int(input('Deseas introducir los palabras que se van a usar como criterior de búsqueda? 1-Si 0-No (Defecto palabras ...): '))
+  if option:
+    search_words = str(input('Introduce las palabras a búscar, en lugar de espacio como separador usa la \',\': ')).split(',')
+
+  option = int(input('Deseas introducir la cantidad de tweets que deseas 1-Si 0-No (Defecto 5 tweets): '))
+  if option:
+    amount_of_results = int(input('Cuantos registros va a querer: '))
+  
+  date_since = input('Introduce la fecha de inicio en la que se va a hacer la búsqueda de tweets (Formato yyyy-mm-dd): ')
+
+  print('\n\nInicializando conexión con Twitter API')
   api = connection()
   if not api.verify_credentials():
-    print('Error in the connection')
+    print('Error en la conexión')
     return False
-  print('Sistem connected with Twitter API')
+  print('Aplicación conectada con Twitter API')
 
   # Collect the tweets
   tweets = tw.Cursor(api.search,
                 q=search_words,
                 lang="en",
-                since=date_since).items(1)
+                since=date_since).items(amount_of_results)
 
-  csvRow = 'Autor;Texto;Fecha\n'
+  csvRow = 'Autor;Texto;Fecha;Pais;Ciudad\n'
 
   # Iterate and print tweets
-  for tweet in tweets:
-    print(tweet.text)
-    csvRow+= f'{str(tweet.user.name)};{str(tweet.text)};{str(tweet.created_at)}\n'
+  for index, tweet in enumerate(tweets):
+    #Assing to the variable tweet the object json that tweet has so we can use it as a dictonary 
+    tweet = tweet._json
+    print(f'Procesado tweet {index+1} de {amount_of_results}')
+    country = 'None'
+    city = 'None'
+    #Validation that help us to know if the tweet has de information of the location
+    if tweet["place"] != None:
+      country = tweet["place"]["country"]
+      city = tweet['place']['name']
+    csvRow+= f'{tweet["user"]["name"]};{tweet["text"]};{tweet["created_at"]};{country};{city}\n'
   archivo = open('tweets.csv', "w")
   archivo.writelines(csvRow)
   archivo.close()
